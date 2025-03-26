ff6f2d4531065aad6d7203ace8a0d6636e392335
tanvir05 <tanvir05.bd@gmail.com>
2025-03-27 00:11:05 +0600

    added all the section

---
+++ script/toggle.js
@@ -0,0 +1,5 @@
+const colorToggleBtn = document.getElementById('colorToggleBtn');
+const body = document.body;
+colorToggleBtn.addEventListener('click', function (){
+  document.body.backGroundColor - "Yellow";
+})
